19dbb2dfad89770033736a9948df8ed3a4a1f7e7
Anthony Coulon <anthony.coulon@valtech.fr>
2014-05-30 16:46:14 +0200

    refactoring calculate score + manage bonus level

--- src/main/java/fr/valtech/bet/service/score/CalculateScoreServiceImpl.java
+++ src/main/java/fr/valtech/bet/service/score/CalculateScoreServiceImpl.java
@@ -37,45 +37,77 @@ public class CalculateScoreServiceImpl implements CalculateScoreService {
         List<User> users = userRepository.findUsers();
         for (User user : users) {
             List<Bet> bets = betRepository.findBetUser(yesterday, user);
-            Integer scoreBet = 0;
+            Integer scoreUser = 0;
             for (Bet bet : bets) {
-                Integer bet1 = Integer.valueOf(bet.getBet().split("-", 2)[0]);
-                Integer bet2 = Integer.valueOf(bet.getBet().split("-", 2)[1]);
-
+                Integer scoreBet = 0;
                 Match match = bet.getMatch();
                 if (StringUtils.isBlank(match.getScore())) {
                     continue;
                 }
-                Integer score1 = Integer.valueOf(match.getScore().split("-", 2)[0]);
-                Integer score2 = Integer.valueOf(match.getScore().split("-", 2)[1]);
 
-                int total = match.getQuote1() + match.getQuote2();
-                int p1 = percent(match.getQuote1(), total);
-                int p2 = percent(match.getQuote2(), total);
-                if (bet1 > bet2 && score1 > score2) {
-                    scoreBet++;
-                    if(p1<45) {
-                        scoreBet+=3;
-                    }
-                } else if (bet1 < bet2 && score1 < score2) {
-                    scoreBet++;
-                    if(p2<45) {
-                        scoreBet+=3;
-                    }
-                } else if (bet1.equals(bet2) && score1.equals(score2)) {
-                    scoreBet++;
-                    if(p1>60 || p2>60) {
-                        scoreBet+=3;
-                    }
-                }
-                if (bet1.equals(score1) && bet2.equals(score2)) {
-                    scoreBet += 2;
-                }
-
+                scoreBet = calculateScore(bet, scoreBet, match);
+                scoreBet = calculateBonusLevel(scoreBet, match);
+                scoreUser+=scoreBet;
             }
-            user.addScore(scoreBet);
+            user.addScore(scoreUser);
             userRepository.save(user);
         }
+    }
+
+    private Integer calculateScore(Bet bet, Integer scoreBet, Match match) {
+        Integer bet1 = Integer.valueOf(bet.getBet().split("-", 2)[0]);
+        Integer bet2 = Integer.valueOf(bet.getBet().split("-", 2)[1]);
+
+        Integer score1 = Integer.valueOf(match.getScore().split("-", 2)[0]);
+        Integer score2 = Integer.valueOf(match.getScore().split("-", 2)[1]);
+
+        int total = match.getQuote1() + match.getQuote2();
+        int p1 = percent(match.getQuote1(), total);
+        int p2 = percent(match.getQuote2(), total);
+
+        if (bet1 > bet2 && score1 > score2) {
+            scoreBet++;
+            if(p1<45) {
+                scoreBet+=3;
+            }
+        } else if (bet1 < bet2 && score1 < score2) {
+            scoreBet++;
+            if(p2<45) {
+                scoreBet+=3;
+            }
+        } else if (bet1.equals(bet2) && score1.equals(score2)) {
+            scoreBet++;
+            if(p1>60 || p2>60) {
+                scoreBet+=3;
+            }
+        }
+        if (bet1.equals(score1) && bet2.equals(score2)) {
+            scoreBet += 2;
+        }
+        return scoreBet;
+    }
+
+    private Integer calculateBonusLevel(Integer scoreBet, Match match) {
+        switch (match.getMatchLevel()) {
+            case eighth:
+                scoreBet*=2;
+                break;
+            case quarter:
+                scoreBet*=3;
+                break;
+            case semi:
+                scoreBet*=4;
+                break;
+            case final2:
+                scoreBet*=5;
+                break;
+            case final1:
+                scoreBet*=6;
+                break;
+            default:
+                break;
+        }
+        return scoreBet;
     }
 
     private int percent(Integer value, int total) {
