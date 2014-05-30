0f6aa358dbe07d330d730ecf7a8f848647369b36
Anthony Coulon <anthony.coulon@valtech.fr>
2014-05-30 19:38:09 +0200

    code correction

--- src/main/java/fr/valtech/bet/domain/model/opponent/Opponent.java
+++ src/main/java/fr/valtech/bet/domain/model/opponent/Opponent.java
@@ -1,6 +1,10 @@
 package fr.valtech.bet.domain.model.opponent;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "OPPONENT")
@@ -14,26 +18,39 @@ public class Opponent {
     @Column(name = "NAME", nullable = false, length = 80)
     private String name;
 
-    @Column(name="GR") // don't put GROUP else there will be an SQL error
+    @Column(name = "GR")
     private String group;
 
     @Column(name = "RANK")
     private byte rank;
 
-    @Column(name="FLAG")
+    @Column(name = "FLAG")
     private String flag;
 
-    public String getFlag(){return flag;}
-    public void setFlag(String flag){this.flag = flag;}
+    public String getFlag() {
+        return flag;
+    }
 
-    public String getGroup() {return group;}
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
     public void setGroup(String group) {
         this.group = group;
     }
 
-    public byte getRank() {return rank;}
+    public byte getRank() {
+        return rank;
+    }
 
-    public void setRank(byte rank) {this.rank = rank;}
+    public void setRank(byte rank) {
+        this.rank = rank;
+    }
+
     public long getId() {
         return id;
     }
@@ -52,13 +69,17 @@ public class Opponent {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
 
         Opponent opponent = (Opponent) o;
 
-        if (id != opponent.id) return false;
-        if (name != null ? !name.equals(opponent.name) : opponent.name != null) return false;
+        if (id != opponent.id)
+            return false;
+        if (name != null ? !name.equals(opponent.name) : opponent.name != null)
+            return false;
 
         return true;
     }
