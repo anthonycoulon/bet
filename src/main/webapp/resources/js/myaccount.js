fc7092fddf01a2407efc59ffa17263575935f219
ftavilla <fabrice.tavilla@gmail.com>
2014-05-31 20:30:39 +0200

    git issue

--- src/main/webapp/resources/js/myaccount.js
+++ src/main/webapp/resources/js/myaccount.js
@@ -8,16 +8,18 @@ MyAccount.prototype.init = function() {
 MyAccount.prototype.saveUser = function () {
 
     var user={};
+
+    
     user.id = $("#id").val();
-    user.firstname = $("#firstname");
-    user.name = $("#name");
+    user.firstName = $("#firstName").val();
+    user.name = $("#name").val();
     user.username = $("#username").val();
     user.currentPassword = $("#currentPassword").val();
     user.newPassword = $("#newPassword").val();
     user.confirmation = $("#confirmation").val();
 
     this.ajax = new Ajax();
-    this.ajax.postData(this.contextUrl+"save", user, bind(this, function(_data) {
+    this.ajax.postData(this.contextUrl+"save", user, bind(this, function() {
         this.ajax.success();
         $("#currentPassword").empty();
         $("#newPassword").empty();
