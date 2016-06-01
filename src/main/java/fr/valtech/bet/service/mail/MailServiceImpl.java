be8f3690c1da65c3c78b261a711871d32cfa7ed4
Anthony Coulon <anthony.coulon@valtech.fr>
2016-06-01 23:23:21 +0200

    set smtp config

--- src/main/java/fr/valtech/bet/service/mail/MailServiceImpl.java
+++ src/main/java/fr/valtech/bet/service/mail/MailServiceImpl.java
@@ -1,22 +1,21 @@
 package fr.valtech.bet.service.mail;
 
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.util.Properties;
-
 @Service
 public class MailServiceImpl  implements MailService{
 
-    public static final String USERNAME = "valtechbet@gmail.com";
+    public static final String USERNAME = "anthony.coulon@valtech.fr";
 
-    public static final String PASSWORD = "ZlatanBet10";
+    public static final String PASSWORD = "";
 
-    public static final String SMTP_HOST = "smtp.gmail.com";
+    public static final String SMTP_HOST = "smtp.office365.com";
 
     public static final String SMTP_HOST_PORT = "587";
 
